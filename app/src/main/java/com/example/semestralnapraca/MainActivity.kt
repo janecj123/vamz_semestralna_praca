87f99ca2f696186e72c467fff2c3f2e93298ff89
Jakub Janec <janecj12345678@gmail.com>
2024-04-28 18:47:07 +0200

    View model pre aplikaciu + uvodna obrazovka

--- app/src/main/java/com/example/semestralnapraca/MainActivity.kt
+++ app/src/main/java/com/example/semestralnapraca/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.example.semestralnapraca
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.semestralnapraca.obrazovky.PrihlasovaciaObrazovka
+import com.example.semestralnapraca.obrazovky.PrihlasovacieOkno
 import data.Podnik
 import data.Tovar
 import data.Zakaznik
 
-
-fun main(){
-    val t1 = Tovar("Pizza",8.35)
-    val t2 = Tovar("Rezne",7.22)
-    val p = Podnik("Mania","Zilina")
-    val z = Zakaznik("Jakub","Janec",26,"0948067869",
-        "Kysucke Nove Mesto","aaa")
-    z.vytvorObjednavku(p)
-    z.pridajDoObjednavky(t1)
-    z.pridajDoObjednavky(t2)
-    z.vypisObjednavku(0)
-
-
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            PrihlasovaciaObrazovka()
+        }
+    }
 }
+//fun main(){
+ //   val t1 = Tovar("Pizza",8.35)
+  //  val t2 = Tovar("Rezne",7.22)
+ //   val p = Podnik("Mania","Zilina")
+ //   val z = Zakaznik("Jakub","Janec",26,"0948067869",
+  //      "Kysucke Nove Mesto","aaa")
+  //  z.vytvorObjednavku(p)
+ //   z.pridajDoObjednavky(t1)
+  //  z.pridajDoObjednavky(t2)
+ //   z.vypisObjednavku(0)
+
+
 
